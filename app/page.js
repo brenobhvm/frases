922a3e56fc4c516e444b8005a0cdefe89031e15f
brenobhvm <brenobhvm@hotmail.com>
2023-05-14 23:38:57 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,92 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function Home({ frases }) {
   return (
     <main>
-      <blockquote>
-        Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn&apos;t really do it, they just saw something. It seemed obvious to them after a while. That&apos;s because they were able to connect experiences they&apos;ve had and synthesize new things.
-        <span>Steve Jobs</span>
-      </blockquote>
+      {frase.map((frase: any) => 
+          <blockquote>
+            {frase.frase}
+            <span>{frase.dia}</span>
+          </blockquote>
+        )
+      
+      }
     </main>
   )
+}
+
+export async function getServerSideProps() {
+  return { props: { 
+    frases: [
+      {
+        frase: "Nunca joguei, mas talvez eu seja uma revelação.",
+        dia: "23-02"
+      },
+      {
+        frase: "Fui induzido a isso",
+        dia: "15-02"
+      },
+      {
+        frase: "Esse bolo tem gosto de chipa",
+        dia: "verificar"
+      },
+      {
+        frase: "Aí sim hein",
+        dia: ""
+      },
+      {
+        frase: "Geralmente eu tenho a linguagem das ruas",
+        dia: "17-03"
+      },
+      {
+        frase: "Isso pode gerar algumas complicações",
+        dia: ""
+      },
+      {
+        frase: "Foi erro da minha parte",
+        dia: "16-02"
+      },
+      {
+        frase: "Eu to investindo na minha carreira",
+        dia: "13-03"
+      },
+      {
+        frase: "Ow, to na merda né?",
+        dia: "22-03"
+      },
+      {
+        frase: "To pensando em me afundar nessa moto aqui",
+        dia: "verificar"
+      },
+      {
+        frase: "Calmô gente, tem Julião pra todo mundo",
+        dia: "17-02"
+      },
+      {
+        frase: "Quem pode, pode. Eu estudei né",
+        dia: "01-03"
+      },
+      {
+        frase: "Sou cria do mato, to acostumado com calor e carrapicho",
+        dia: "01-03"
+      },
+      {
+        frase: "Com um celta eu vou para qualquer lugar",
+        dia: "08-03"
+      },
+      {
+        frase: "Pai tá voando!",
+        dia: "13-03"
+      },
+      {
+        frase: "Ela tem que fazer ran dan dan dan",
+        dia: "29-03"
+      },
+      {
+        frase: "Eu tenho mel, isso não da pra negar",
+        dia: "05-04"
+      },
+    ]
+  } };
 }
